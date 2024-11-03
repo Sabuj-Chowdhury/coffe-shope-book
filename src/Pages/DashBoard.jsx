7c30fc25710a536334b7bdea73b6e32ed1f49ac2
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-03 20:30:15 +0530

    DashBoard add from local stroage

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import Heading from "../Components/Heading";
+import { getAllFavorite } from "../Util";
+import Cards from "../Components/Cards";
+
 const DashBoard = () => {
+  const [coffees, setCoffees] = useState([]);
+  useEffect(() => {
+    const favorites = getAllFavorite();
+    setCoffees(favorites);
+  }, []);
   return (
     <div>
-      <p className="text-center text-4xl">Dashboard page</p>
+      <Heading
+        title={"Welcome to Dashboard"}
+        subtitle={
+          "Manage Coffees that you have previously added as favorites. You Can add or remove them from here."
+        }
+      ></Heading>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
+        {coffees.map((coffee) => (
+          <Cards key={coffee.id} coffee={coffee}></Cards>
+        ))}
+      </div>
     </div>
   );
 };
