5aab12c7026cf4624be6e5e8453ab96396031972
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-03 16:09:55 +0530

    Category tab add

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+const Categories = ({ categories }) => {
+  // console.log(categories);
+  return (
+    <div role="tablist" className="tabs tabs-lifted">
+      {categories.map((category) => (
+        <Link key={category.category} to="/cards" role="tab" className="tab">
+          {category.category}
+        </Link>
+      ))}
+    </div>
+  );
+};
+Categories.propTypes = {
+  categories: PropTypes.array,
+};
+
+export default Categories;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,11 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../Components/Banner";
 import Heading from "../Components/Heading";
+import Categories from "../Components/Categories";
 
 const Home = () => {
+  const categories = useLoaderData();
+  // console.log(categories);
   return (
     <div>
       {/* Banner */}
@@ -16,6 +20,7 @@ const Home = () => {
       ></Heading>
 
       {/* categories tab section */}
+      <Categories categories={categories}></Categories>
 
       {/* dynamic nested components */}
     </div>
