88ae1a4e564fca9ee98011c3b7cc02e79b327a40
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-03 12:35:37 +0530

    File structure add for Components,Routes,Layouts,Pages and file add, Footer and navbar Component add form daisyUI (Need Customisation)

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+const MainLayout = () => {
+  return (
+    <div>
+      {/* Navbar Thakbe */}
+
+      {/* Dynamic sections thakbe */}
+
+      {/* Footer thakbe */}
+    </div>
+  );
+};
+
+export default MainLayout;
