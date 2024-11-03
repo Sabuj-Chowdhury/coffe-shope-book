a857bc344f9d73c4ec7dedb45c34915f74347403
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-11-03 12:52:26 +0530

    MainLayout min-h add to show nav at the top and footer at the bottom

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
+import NavBar from "../Components/NavBar";
+
 const MainLayout = () => {
   return (
     <div>
       {/* Navbar Thakbe */}
+      <NavBar></NavBar>
 
       {/* Dynamic sections thakbe */}
+      <div className="min-h-[calc(100vh-232px)] py-12">
+        <Outlet></Outlet>
+      </div>
 
       {/* Footer thakbe */}
+      <Footer></Footer>
     </div>
   );
 };
